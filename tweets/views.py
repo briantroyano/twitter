f32f3855bd3c0d7a14a0de8434d9d029809014cc
briantroyano <briantroyano12@gmail.com>
2020-08-18 21:09:59 -0500

    Like Button

--- tweets/views.py
+++ tweets/views.py
@@ -5,14 +5,30 @@ from .models import Tweet
 # Create your views here.
 def home_view(request, *args,**kwargs):
     
-    return HttpResponse("<h1>Hello Brian</h1>")
+    # return HttpResponse("<h1>Hello Brian</h1>")
+    return render(request, 'pages/home.html', context={}, status=200)
+
+
+def tweet_list_view(request, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JavaScript or Swift or Java or iOS/Android
+    return json data
+    """
+    qs = Tweet.objects.all()
+    tweets_list = [{'id': x.id, 'content': x.content} for x in qs]
+    data = {
+        'isUser': False,
+        'response':tweets_list
+    }
+    return JsonResponse(data)
+
 
 def tweet_detail_view(request, tweet_id, *args,**kwargs):
     """
     REST API VIEW
     Consume by JavaScript or Swift or Java or iOS/Android
     return json data
-    
     """
     
     data = {
