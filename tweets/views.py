d13f930cc36630b5bd742eba89f583bbdec2a23a
briantroyano <briantroyano12@gmail.com>
2020-08-18 22:35:01 -0500

    correct count

--- tweets/views.py
+++ tweets/views.py
@@ -1,46 +1,43 @@
-from django.shortcuts import render
+import random
 from django.http import HttpResponse, Http404, JsonResponse
+from django.shortcuts import render
+
 from .models import Tweet
 
 # Create your views here.
-def home_view(request, *args,**kwargs):
-    
-    # return HttpResponse("<h1>Hello Brian</h1>")
-    return render(request, 'pages/home.html', context={}, status=200)
+def home_view(request, *args, **kwargs):
+    return render(request, "pages/home.html", context={}, status=200)
 
 
 def tweet_list_view(request, *args, **kwargs):
     """
     REST API VIEW
-    Consume by JavaScript or Swift or Java or iOS/Android
+    Consume by JavaScript or Swift/Java/iOS/Andriod
     return json data
     """
     qs = Tweet.objects.all()
-    tweets_list = [{'id': x.id, 'content': x.content} for x in qs]
+    tweets_list = [{"id": x.id, "content": x.content, "likes": random.randint(0, 122)} for x in qs]
     data = {
-        'isUser': False,
-        'response':tweets_list
+        "isUser": False,
+        "response": tweets_list
     }
     return JsonResponse(data)
 
 
-def tweet_detail_view(request, tweet_id, *args,**kwargs):
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
     """
     REST API VIEW
-    Consume by JavaScript or Swift or Java or iOS/Android
+    Consume by JavaScript or Swift/Java/iOS/Andriod
     return json data
     """
-    
     data = {
-        'id': tweet_id,
-        # 'image_path': tweet.image.url
+        "id": tweet_id,
     }
     status = 200
     try:
-        tweet = Tweet.objects.get(id=tweet_id)
-        data['content'] = tweet.content
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
     except:
-        data['message'] = 'Not Found'
+        data['message'] = "Not found"
         status = 404
-    return JsonResponse(data,status=status) # json.dumps content='application.json'
-    # return HttpResponse(f"<h1>Hello {tweet_id} - {tweet.content}</h1>")
+    return JsonResponse(data, status=status) # json.dumps content_type='application/json'
